0892a257cf46820b41728d337442fdf932e690a0
Toodle <alicebobo223@gmai.com>
2022-09-28 21:17:35 +0800

    幫我做直播  拿著 照我 師傅確定要拍嗎? 確定要拍嗎? 我中了兩槍....希望大家 如果我這一次不幸死的話 請大家一定要傳承我的精神一定要傳承我的精神  啊啊啊啊啊啊啊啊 請大家照顧我的老婆跟小孩拜託大家 拜託大家還有我的媽媽 拜託大家  啊啊啊啊啊啊啊啊 如果我我不幸死的話一定要傳承我的精神 一定要傳承我的精RRRRRRRRR如果我我不幸死的話 請大家照顧我的老婆跟小孩 還有我的我媽媽RRRRRRRRR

---
+++ 111-1QZ1/Bomb.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ1
+{
+    public partial class Bomb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] ia_Mindex = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
+            char[,] ia_Map = new char[10, 10];
+            for (int i_Row = 0; i_Row < 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                  ia_Map[i_Row, i_Col] = '0';
+                }
+            }
+            for (int i_Ct = 0; i_Ct < 10; i_Ct++)
+            {
+                int i_Row = ia_Mindex[i_Ct] / 10;
+                int i_Col = ia_Mindex[i_Ct] % 10;
+                ia_Map[i_Row, i_Col] = '*';
+            }
+            for (int i_Row = 0; i_Row < 10; i_Row++)
+            {
+                for (int i_Col = 0; i_Col < 10; i_Col++)
+                {
+                    if (ia_Map[i_Row, i_Col] == ' ')
+                    {
+                        Response.Write("&nbsp");
+                    }
+                    else
+                    {
+                        Response.Write(ia_Map[i_Row, i_Col]);
+                    }
+                }
+                Response.Write("<br />");
+            }
+        }
+    }
+}
